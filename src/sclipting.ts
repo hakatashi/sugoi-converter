c94595eaaaacfa1863cf934e7cc3294932f2ad15
Koki Takahashi <hakatasiloving@gmail.com>
2015-11-10 13:40:59 +0900

    Reuse chr function in Sclipting converter

--- src/sclipting.ts
+++ src/sclipting.ts
@@ -1,7 +1,7 @@
 require('core-js/fn/string/code-point-at');
-require('core-js/fn/string/from-code-point');
 
 import assert = require('assert');
+import {ord, chr} from './util'
 
 export const encode = (data:Buffer) => {
 	let text = '';
@@ -14,13 +14,13 @@ export const encode = (data:Buffer) => {
 		assert(1 <= bytes.length && bytes.length <= 3);
 
 		if (bytes.length === 1) {
-			remnant = String.fromCodePoint(0xAC00 + (bytes[0] << 4));
+			remnant = chr(0xAC00 + (bytes[0] << 4));
 		} else if (bytes.length === 2) {
-			remnant = String.fromCodePoint(0xAC00 + (bytes[0] << 4) + (bytes[1] >> 4));
-			remnant += String.fromCodePoint(0xBC00 + (bytes[1] & 0x0F));
+			remnant = chr(0xAC00 + (bytes[0] << 4) + (bytes[1] >> 4));
+			remnant += chr(0xBC00 + (bytes[1] & 0x0F));
 		} else {
-			remnant = String.fromCodePoint(0xAC00 + (bytes[0] << 4) + (bytes[1] >> 4));
-			remnant += String.fromCodePoint(0xAC00 + ((bytes[1] & 0x0F) << 8) + bytes[2]);
+			remnant = chr(0xAC00 + (bytes[0] << 4) + (bytes[1] >> 4));
+			remnant += chr(0xAC00 + ((bytes[1] & 0x0F) << 8) + bytes[2]);
 		}
 
 		text += remnant;
@@ -38,17 +38,17 @@ export const decode = (text:string) => {
 	let ptr:number;
 	for (ptr = 0; ptr < text.length; ) {
 		const codePoint = text.codePointAt(ptr);
-		ptr += String.fromCodePoint(codePoint).length;
+		ptr += chr(codePoint).length;
 
 		if (codePoint < 0xAC00 || 0xBC00 <= codePoint) {
-			throw new Error(`Invalid character: ${String.fromCodePoint(codePoint)}`);
+			throw new Error(`Invalid character: ${chr(codePoint)}`);
 		}
 
 		// remaining byte is 1
 		if (text.length <= ptr) {
 			// NOTE: This check can be skipped
 			if ((codePoint & 0x0F) !== 0) {
-				throw new Error(`Invalid character: ${String.fromCodePoint(codePoint)}`);
+				throw new Error(`Invalid character: ${chr(codePoint)}`);
 			}
 
 			bytes.push((codePoint - 0xAC00) >> 4);
@@ -56,7 +56,7 @@ export const decode = (text:string) => {
 		}
 
 		const codePoint2 = text.codePointAt(ptr);
-		ptr += String.fromCodePoint(codePoint2).length;
+		ptr += chr(codePoint2).length;
 
 		if (0xAC00 <= codePoint2 && codePoint2 < 0xBC00) {
 			bytes.push((codePoint - 0xAC00) >> 4);
@@ -67,7 +67,7 @@ export const decode = (text:string) => {
 			bytes.push((((codePoint - 0xAC00) & 0x0F) << 4) | (codePoint2 - 0xBC00));
 			break;
 		} else {
-			throw new Error(`Invalid character: ${String.fromCodePoint(codePoint2)}`);
+			throw new Error(`Invalid character: ${chr(codePoint2)}`);
 		}
 	}
 
