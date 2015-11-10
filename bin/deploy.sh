bccfc4f2ae5f44a0d60b8403d3ac06c0e2959506
Travis CI <hakatasiloving@gmail.com>
2015-11-10 22:49:05 +0900

    Add deploy script

---
+++ bin/deploy.sh
@@ -0,0 +1,28 @@
+if test "$TRAVIS_SECURE_ENV_VARS" = "true"; then
+
+	NPM_BIN=`npm bin`
+
+	# Set identification
+	git config user.name "Travis CI"
+	git config user.email "hakatasiloving@gmail.com"
+
+	# Discard any changes in build
+	git checkout -- .
+
+	# Checkout and merge
+	git checkout gh-pages
+	git merge master
+
+	# Build release files
+	$NPM_BIN/gulp release
+
+	# Create commit
+	git add index.html
+	git add index.min.js
+	git add index.min.css
+	git commit -m "Update build"
+
+	# Push it all
+	git push "https://${GH_TOKEN}@github.com/hakatashi/sugoi-converter.git" gh-pages:gh-pages > /dev/null 2>&1
+
+fi
