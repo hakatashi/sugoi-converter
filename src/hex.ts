a1a9faf793fc9288a3afd01f8dee6b03800e1110
Travis CI <hakatasiloving@gmail.com>
2016-04-30 14:05:27 +0900

    Manually validate hex string for Node.js v6

--- src/hex.ts
+++ src/hex.ts
@@ -3,5 +3,9 @@ export const encode = (data:Buffer) => {
 };
 
 export const decode = (text:string) => {
+	if (text.match(/[^0-9a-f\s]/i)) {
+		throw new Error('Invalid hex string');
+	}
+
 	return new Buffer(text.replace(/\s/g, ''), 'hex');
 };
